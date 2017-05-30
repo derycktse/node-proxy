52829abd1118d3bd127c47c477bd991287aa5231
xiedangang <285205451@qq.com>
2017-05-31 00:37:30 +0800

    init file

---
+++ README.md
@@ -0,0 +1,8 @@
+A simple node proxy server
+
+
+## usage ##
+
+```
+http://localhost:8080/?get=https://www.baidu.com
+```
---
+++ index.js
@@ -0,0 +1,39 @@
+let http = require('http'),
+    url = require('url')
+
+let PORT = process.env.PORT || 8080
+
+http.createServer((req, res) => {
+    let pUrl = url.parse(req.url, true)
+
+    if (!pUrl.query || !pUrl.query.get) {
+        res.writeHead(404, "text/plain")
+        res.end("url not found!")
+        return
+    }
+
+    let destUrl = url.parse(pUrl.query.get, true)
+
+    let proxyRequest = http.request({
+        port: destUrl.port || 80,
+        host: destUrl.host,
+        method: 'GET',
+        path: destUrl.path
+    })
+
+    proxyRequest.end()
+
+    proxyRequest.addListener('response', (proxyResponse) => {
+        res.writeHead(proxyResponse.statusCode, proxyResponse.headers)
+
+        proxyResponse.addListener('data', (chunk) => {
+            res.write(chunk)
+        })
+
+        proxyResponse.addListener('end', () => {
+            res.end()
+        })
+    })
+}).listen( PORT, ()=>{
+    console.log(`server is listening at ${PORT}`)
+} )
